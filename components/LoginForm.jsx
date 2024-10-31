403869606fb13b37273579105d4c38cb3033ee0a
yousseflearn <youssefeasylearn@gmail.com>
2024-10-31 19:12:56 +0400

    Mongo Setup && SignUp api and SignUp  frontend working together

--- components/LoginForm.jsx
+++ components/LoginForm.jsx
@@ -1,6 +1,15 @@
+'use client';
+import { useState, useEffect } from 'react';
 import Input from '@/components/Input';
 import Link from 'next/link';
 const LoginForm = () => {
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+  if (!hydrated) {
+    return null;
+  }
   return (
     <section className="container">
       <form className="border-2 border-paragraphColor rounded-lg max-w-sm mx-auto px-8 py-6 space-y-5">
---
+++ models/User.js
@@ -0,0 +1,41 @@
+import { timeStamp } from 'console';
+import mongoose from 'mongoose';
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    designation: {
+      type: String,
+      default: '',
+    },
+    avatar: {
+      type: Object,
+      default: {},
+    },
+    age: {
+      type: String,
+      default: '',
+    },
+    location: {
+      type: String,
+      default: '',
+    },
+    about: {
+      type: String,
+      default: '',
+    },
+  },
+  { timeStamp: true }
+);
+export default mongoose.models?.User || mongoose.model('User', UserSchema);
